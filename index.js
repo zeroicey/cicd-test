b2fe0b53d8f03c30ecd199a044051cd038266181
zeroicey <zeroicey.hp@outlook.com>
2026-05-21 15:13:23 +0800

    feat: change index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello cicd");
+  res.send("hello cicd world");
 });
 
 app.listen(port, () => {
